65d3d16016e45f292f35c81d849f29180e17640b
Christian González <christian.gonzalez@nerdocs.at>
2020-01-22 10:42:50 +0100

    better prompt for examples

--- prompts.js
+++ prompts.js
@@ -2,7 +2,7 @@ module.exports = [
   {
     name: `addExample`,
     type: 'confirm',
-    message: 'Add example plugin/components?',
+    message: 'Add an example extension and component to project?',
     default: false,
   },
 ];
